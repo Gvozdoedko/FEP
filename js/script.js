993364fb8c2ef43bbcb5d5842d2b1937a162a49f
Gvozdoedko <uraradhenko007@gmail.com>
2022-04-19 23:14:18 +0300

    greetings and age

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 alert('Hello!');
 let name = prompt('What is your name?');
+
 alert('Nice to meet you, ' + name);
+
+let age = prompt('how old are you?');
+const currentYear = 2022;
+alert(currentYear - age);
+
 
 
 
